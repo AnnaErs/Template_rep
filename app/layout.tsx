7fa10548a4745610dda290569ca73aaef7cfd319
AnnaErs <annaershova02@gamil.com>
2023-02-15 23:39:45 +0500

    feat: BIKDARTEM-54: [FRONTEND] Создать шаблон проекта в гитхаб

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,20 @@
-import './globals.css'
+import "./globals.css";
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
-      <body>{children}</body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            <head>
+                <meta
+                    content="width=device-width, initial-scale=1"
+                    name="viewport"
+                />
+                <link rel="icon" href="/favicon.ico" />
+            </head>
+            <body>{children}</body>
+        </html>
+    );
 }
